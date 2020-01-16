7dfa748b16a29c521fba78448f3b885597ff6ed8
fnando <fnando.gomez@gmail.com>
2020-01-16 00:50:49 -0600

    Inital commit, testing
    Testing

---
+++ API_exe_Ghibli_1.0/scripts.js
@@ -0,0 +1,45 @@
+
+fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response =>{
+        return response.json()
+    })
+    .then(data =>{
+        data.forEach(movie => { 
+            
+            //Create a div with a card class
+            const card = document.createElement('div')
+            card.setAttribute('class','card')
+
+            //Create an "h1" and set the text content to the film's title
+            const h1 = document.createElement('h1')
+            h1.textContent = movie.title
+
+            //Create a "p" and set the text content to the film's description
+            const p = document.createElement('p')
+            movie.description = movie.description.substring(0,300)// Limit to 300 chars
+            p.textContent = `${movie.description}...`
+
+            container.appendChild(card)
+
+            card.appendChild(h1)
+            card.appendChild(p)
+            
+        });
+    })
+    .catch(err=>{
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = "Oops, something isn't working in the Ghibli studio"
+
+        app.appendChild(errorMessage)
+    })
+
+    const app = document.getElementById('root')
+
+    const logo = document.createElement('img')
+        logo.src = 'logo.png'
+
+    const container = document.createElement('div')
+    container.setAttribute('class', 'container')
+
+    app.appendChild(logo)
+    app.appendChild(container)
